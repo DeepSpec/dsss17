b22d8d8c5c2b8b6afefcf25b9d1335b4b0727893
Benjamin Pierce <bcpierce00@gmail.com>
2017-07-16 20:05:52 -0400

    Add Vellvm.v

---
+++ qc/Vellvm.v
@@ -0,0 +1,9 @@
+(** * Vellvm: A Larger Case Study *)
+
+(** Our final case study on property-based testing is in the context
+    of [Vminus], a compiler for [Imp] built on a simplified version of
+    the [Vellvm] framework for verified compilation in the LLVM
+    ecosystem.
+
+    To get started, visit the [vminus] directory in the [dsss17]
+    repository and open the file [CompilerTest]. *)
